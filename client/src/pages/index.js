c8e81af5752f6d7b80df69a595e8cc202d16693c
Felipe <87789520+FelipeSilva916@users.noreply.github.com>
2022-12-28 15:35:35 -0800

    video 8

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,12 +1,39 @@
-import React, { Fragment } from 'react';
-import { Router } from '@reach/router';
+import React, { Fragment } from "react";
+import { Router } from "@reach/router";
 /** importing our pages */
-import Tracks from './tracks';
+import Tracks from "./tracks";
+
+import Track from "./track";
+
+const GET_TRACK = gql`
+  query GetTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      numberOfViews
+      modules {
+        id
+        title
+        length
+      }
+      description
+    }
+  }
+`;
 
 export default function Pages() {
   return (
     <Router primary={false} component={Fragment}>
       <Tracks path="/" />
+      <Track path="/track/:trackId" />
     </Router>
   );
 }
