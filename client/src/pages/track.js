7484605df5653a97479b6d73c62c48dc71c7b069
Felipe <87789520+FelipeSilva916@users.noreply.github.com>
2023-01-04 15:12:09 -0800

    Update track.js

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -3,6 +3,10 @@ import { gql, useQuery } from "@apollo/client";
 import { Layout, QueryResult } from "../components";
 
 const Track = ({ trackId }) => {
+  const { loading, error, data } = useQuery(GET_TRACK, {
+    variables: { trackId }
+  });
+
   return <Layout></Layout>;
 };
 
